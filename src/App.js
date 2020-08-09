11c9994d0f7110656ee65563eb6fc5a17922a4ad
zixiiu <faznky@gmail.com>
2020-08-09 16:43:52 -0400

    test commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Zixiu's commit
         </p>
         <a
           className="App-link"
